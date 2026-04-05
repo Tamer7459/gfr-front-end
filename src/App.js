c6b44053c7c63663c33c995519ee3cb0232a8277
Tamer <tamerinale@gmail.com>
2026-04-05 02:20:06 +0100

    front-end

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// src/App.js
 import { useMemo, useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from '@mui/material';
@@ -7,15 +8,17 @@ import { useTranslation } from 'react-i18next';
 import { cacheLtr, cacheRtl } from './i18n/emotionCache';
 import { createAppTheme } from './theme/createAppTheme';
 import { AuthProvider } from './context/AuthContext';
-import AppRoutes from './routes/AppRoutes';
 import { ThemeContextProvider } from './context/ThemeContext';
+import AppRoutes from './routes/AppRoutes';
 
-function AppShell() {
+function App() {
   const { i18n, t } = useTranslation();
   const isRtl = i18n.language === 'ar';
 
+  // إنشاء Theme حسب اللغة
   const theme = useMemo(() => createAppTheme(isRtl ? 'rtl' : 'ltr'), [isRtl]);
 
+  // تغيير عنوان الصفحة تلقائيًا عند تغيير اللغة
   useEffect(() => {
     document.title = t('app.title');
   }, [i18n.language, t]);
@@ -24,21 +27,15 @@ function AppShell() {
     <CacheProvider value={isRtl ? cacheRtl : cacheLtr}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <AuthProvider>
-          <AppRoutes />
-        </AuthProvider>
+        <BrowserRouter>
+          <ThemeContextProvider>
+            <AuthProvider>
+              <AppRoutes />
+            </AuthProvider>
+          </ThemeContextProvider>
+        </BrowserRouter>
       </ThemeProvider>
     </CacheProvider>
-  );
-}
-
-function App() {
-  return (
-    <BrowserRouter>
-      <ThemeContextProvider>
-        <AppShell />
-      </ThemeContextProvider>
-    </BrowserRouter>
   );
 }
 
